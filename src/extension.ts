35259be1c51e4bcf31703f183a5d4e7746b65468
Joakim Riedel <joakim.riedel@gmail.com>
2025-01-17 17:20:48 +0100

    first working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,60 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Register the command
+  const disposable = vscode.commands.registerCommand(
+    "extension.bundle-tabs",
+    async () => {
+      try {
+        // Get all tabs across all tab groups
+        const allTabs = vscode.window.tabGroups.all.flatMap(
+          (group) => group.tabs
+        );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bundle-tabs" is now active!');
+        // We'll build a big string here, start with current day and time
+        let mergedContent = `# ${new Date().toLocaleString()}\n`;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('bundle-tabs.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from bundle-tabs!');
-	});
+        // Process each tab to see if it's a text file
+        for (const tab of allTabs) {
+          if (tab.input instanceof vscode.TabInputText) {
+            // Open the TextDocument to access content
+            const doc = await vscode.workspace.openTextDocument(tab.input.uri);
 
-	context.subscriptions.push(disposable);
+            // Build a relative path for the header
+            let relativePath = doc.fileName;
+            if (
+              vscode.workspace.workspaceFolders &&
+              vscode.workspace.workspaceFolders.length > 0
+            ) {
+              const rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+              relativePath = relativePath.replace(rootPath, "");
+            }
+
+            // Use doc.languageId to generate the fenced code block language
+            const language = doc.languageId;
+
+            mergedContent += `\n## ${relativePath}\n\n`;
+            mergedContent += `\`\`\`${language}\n`;
+            mergedContent += doc.getText();
+            mergedContent += `\n\`\`\`\n`;
+          }
+        }
+
+        // Open the merged content in a new, unsaved document
+        const newDoc = await vscode.workspace.openTextDocument({
+          content: mergedContent,
+          language: "markdown",
+        });
+        await vscode.window.showTextDocument(newDoc, { preview: false });
+      } catch (err) {
+        vscode.window.showErrorMessage(`Error merging files: ${err}`);
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  // Clean up if needed
+}
